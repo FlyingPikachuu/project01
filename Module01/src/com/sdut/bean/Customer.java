7c3a0726019f96ada4b28a556c0238de0c831f7d
FlyingPikachuu <44453026+FlyingPikachuu@users.noreply.github.com>
2022-03-01 10:42:39 +0800

    Initial commit

---
+++ Module01/src/com/sdut/bean/Customer.java
@@ -0,0 +1,7 @@
+package com.sdut.bean;
+
+public class Customer {
+    public static void main(String[] args){
+        System.out.println("helloworld！！");
+    }
+}
---
+++ src/com/sdut/java/hello.java
@@ -0,0 +1,8 @@
+package com.sdut.java;
+
+/**
+ * @author shkstart
+ * @create 2022/2/27 - 18:06
+ */
+public class hello {
+}
