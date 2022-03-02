1f23ad7b8a62be257325238446a8c80e19746911
FlyingPikachuu <297769391@qq.com>
2022-03-02 19:46:27 +0800

    x4

--- Module01/src/com/sdut/bean/Customer.java
+++ Module01/src/com/sdut/bean/Customer.java
@@ -2,6 +2,10 @@ package com.sdut.bean;
 
 public class Customer {
     public static void main(String[] args){
+
+        System.out.println("helloworld！！");
+        System.out.println("helloworld！！");
+        System.out.println("helloworld！！");
         System.out.println("helloworld！！");
     }
 }
